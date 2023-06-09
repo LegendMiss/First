94ee767eec4aaf82e2b50bd2b89cb846688fcd4f
linmutang <tangpengcong@xiaomi.com>
2023-06-09 21:20:04 +0800

    commit lib

---
+++ module-login/src/main/java/com/vola/test_login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.vola.test_login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vola.module_login.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
